719f978817fcca3cdde348f30fc17581ae9caabe
TortleTurtle <danielmeijs@gmail.com>
2023-05-18 15:44:23 +0200

    Added Markdown parsing and changed styling to accomodate this.

--- src/components/TextBlock.js
+++ src/components/TextBlock.js
@@ -1,11 +1,12 @@
 import styles from '@/styles/TextBlock.module.scss'
+import ReactMarkdown from "react-markdown";
 
 export default function TextBlock({title, subtitle, description}) {
     return <section className={styles.textBlock}>
         <div className={styles.container}>
             <h2>{title}</h2>
             {subtitle && <h3>{subtitle}</h3>}
-            <p>{description}</p>
+            <ReactMarkdown>{description}</ReactMarkdown>
         </div>
     </section>;
 }
